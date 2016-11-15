3fbd04bc7a93ce4b25aa3921c223d4ed3a423404
Rohit Misra <rohitmisra.44@gmail.com>
2016-11-15 15:49:12 +0100

    roll function separated

--- dice.js
+++ dice.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
 
+function roll(){
+	return Math.floor(Math.random()*(6-1+1)+1);
+}
+
 app.get('/', function (req, res) {
   res.send('please goto /roll');
 });
 
 app.get('/roll',function(req, res){
-  res.send(''+Math.floor(Math.random()*(6-1+1)+1));
+  res.send(''+ roll());
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Rolling dice app listening on port 3000!');
 });
